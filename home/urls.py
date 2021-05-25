eb5f2073e827ffb1bde2e93b9eb3636cc3448791
shivamj2200 <shivamjaiswal2200@gmail.com>
2021-05-25 19:16:20 +0530

    first commit

---
+++ home/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls  import path
+from .views import*
+urlpatterns = [
+       path('', home , name="home"),
+       path('api/hotels', api_hotels)
+]
+# Create your views here.
